a80e433f2900b2ea60d2ab265154fb7b2163ce4f
Chris Marchesi <chrism@vancluevertech.com>
2017-01-08 18:46:41 -0800

    Add the message function in directly

--- release.sh
+++ release.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
-#shellcheck disable=SC1090
-source "$(dirname "$0")/message.function"
-
 # The release from CHANGELOG.md.
 release=$(head -n 1 CHANGELOG.md | awk '{print $2}')
+
+# message prints text with a color, redirected to stderr in the event of
+# warning or error messages.
+message() {
+  declare -A __colors=(
+    ["error"]="31"   # red
+    ["warning"]="33" # yellow
+    ["begin"]="32"   # green
+    ["ok"]="32"      # green
+    ["info"]="1"     # bold
+    ["reset"]="0"    # here just to note reset code
+  )
+  local __type="$1"
+  local __message="$2"
+  if [ -z "${__colors[$__type]}" ]; then
+    __type="info"
+  fi
+  if [[ ! "${__type}" =~ ^(warning|error)$ ]]; then
+    echo -e "\e[${__colors[$__type]}m${__message}\e[0m" 1>&2
+  else
+    echo -e "\e[${__colors[$__type]}m${__message}\e[0m"
+  fi
+}
 
 if [[ "${release}" == *"-pre" ]]; then
   message error "This is a pre-release - release aborted." >&2
